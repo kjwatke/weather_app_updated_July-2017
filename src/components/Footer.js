b49a6a48a8cd3a93ba7c2754971a29e5083f490c
kjwatke@gmail.com <a7cfZih4J2BY>
2017-07-07 11:51:34 -0700

    added comment to add CC commons license at a later point.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,13 +4,26 @@ import { Navbar, Nav } from 'react-bootstrap';
 const Footer = () =>
   (<footer>
     <Navbar inverse fixedBottom>
-      <Nav
+      {/* <Nav
         style={{
           display: 'flex',
-          flexFlow: 'nowrap row',
+          flexFlow: 'wrap row',
           justifyContent: 'space-between',
         }}
-      />
+      >
+        <a href="#">link</a>
+        <a href="#">link</a>
+        <a rel="license" href="http://creativecommons.org/licenses/by/2.0/">
+          <img
+            alt="Creative Commons License"
+            style={{ borderWidth: '0' }}
+            src="https://i.creativecommons.org/l/by/2.0/80x15.png"
+          />
+        </a>
+        <a rel="license" href="http://creativecommons.org/licenses/by/2.0/">
+          Creative Commons Attribution 2.0 Generic License
+        </a>
+      </Nav> */}
     </Navbar>
   </footer>);
 
